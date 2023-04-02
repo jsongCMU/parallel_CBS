fc824d1d8bd87c9165596f7b911f988e1b77a20d
Jaekyung Song <jaekyuns@andrew.cmu.edu>
2023-04-01 23:33:04 -0400

    Created test script for AStar

--- code/src/testAStar.cpp
+++ code/src/testAStar.cpp
@@ -1,4 +1,3 @@
-#include <cstdio>
 #include "MAPFLoader.hpp"
 #include "AStar.hpp"
 
@@ -10,5 +9,35 @@ int main()
     std::string fileName = "./instances/random_map.txt";
     MAPFInstance mapfProblem = loader.loadInstanceFromFile(fileName);
 
-    // Run specific version of CBS
+    // Display map
+    printf("Map:\n");
+    for(int i=0; i<mapfProblem.rows; i++){
+        for(int j=0; j<mapfProblem.cols; j++){
+            printf("%s", mapfProblem.map[i][j] ? "@ ": ". ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    // Create A* solver
+    AStar SAPFsolver;
+
+    // Solve Problem
+    int failCount=0;
+    printf("Solver:\n");
+    for(int i = 0; i < mapfProblem.numAgents; i++)
+    {
+        bool succ = SAPFsolver.solve(mapfProblem, i);
+        if(!succ){
+            printf("* Failed to solve for agent %d: (%d,%d) -> (%d,%d)\n", i, mapfProblem.startLocs[i].x, mapfProblem.startLocs[i].y, mapfProblem.goalLocs[i].x, mapfProblem.goalLocs[i].y);
+            failCount++;
+            continue;
+        }
+        printf("Found solution for agent %d:\n\t", i);
+        std::vector<Point2> path = SAPFsolver.getPath();
+        for(int j=0; j<path.size(); j++)
+            printf("(%d, %d), ", path[j].x, path[j].y);
+        printf("\n");
+    }
+    printf("Result: passed %d/%d\n", mapfProblem.numAgents-failCount, mapfProblem.numAgents);
 }
