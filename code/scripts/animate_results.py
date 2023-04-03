6c6ee67788cbe9abb4c54995b9adfce902f1f391
Jaekyung Song <jaekyuns@andrew.cmu.edu>
2023-04-02 23:32:15 -0400

    Adjust for running from build

--- code/scripts/animate_results.py
+++ code/scripts/animate_results.py
@@ -2,13 +2,7 @@
 import argparse
 import glob
 from pathlib import Path
-# from cbs import CBSSolver
-# from pbs import PBSSolver
-# from independent import IndependentSolver
-# from joint_state import JointStateSolver
-# from prioritized import PrioritizedPlanningSolver
 from visualize import Animation
-# from single_agent_planner import get_sum_of_cost
 
 SOLVER = "CBS"
 
@@ -99,38 +93,6 @@ def import_results(filename):
         path = list(map(lambda loc: tuple(map(int, loc.split(','))), path))
         paths.append(path)
         line = " ".join(f.readline().split())
-    # rows, columns = [int(x) for x in line.split(' ')]
-    # rows = int(rows)
-    # columns = int(columns)
-    # # #rows lines with the map
-    # my_map = []
-    # for r in range(rows):
-    #     line = f.readline()
-    #     my_map.append([])
-    #     for cell in line:
-    #         if cell == '@':
-    #             my_map[-1].append(True)
-    #         elif cell == '.':
-    #             my_map[-1].append(False)
-    # # #agents
-    # line = f.readline()
-    # num_agents = int(line)
-    # # #agents lines with the start/goal positions
-    # starts = []
-    # goals = []
-    # for a in range(num_agents):
-    #     line = f.readline()
-    #     sx, sy, gx, gy = [int(x) for x in line.split(' ')]
-    #     if my_map[sx][sy]:
-    #        raise BaseException("Agent {} start postion {} {} in collision".format(a, sx, sy)) 
-    #     if my_map[gx][gy]:
-    #        raise BaseException("Agent {} goal postion {} {} in collision".format(a, gx, gy)) 
-    #     if (sx, sy) in starts:
-    #        raise BaseException("Agent {} start postion {} {} repeated".format(a, sx, sy)) 
-    #     if (gx, gy) in goals:
-    #        raise BaseException("Agent {} goal postion {} {} repeated".format(a, gx, gy)) 
-    #     starts.append((sx, sy))
-    #     goals.append((gx, gy))
     f.close()
     return mapf_filename, soc, paths
 
@@ -138,7 +100,7 @@ def import_results(filename):
 if __name__ == '__main__':
     # Get arguments
     parser = argparse.ArgumentParser(description='Create animation using results file')
-    parser.add_argument('--file', type=str, default='outputs/result.txt',
+    parser.add_argument('--file', type=str, default='../outputs/result.txt',
                         help='The name of the file to animate')
     args = parser.parse_args()
 
@@ -151,4 +113,4 @@ if __name__ == '__main__':
 
     print("***Test paths on a simulation***")
     animation = Animation(my_map, starts, goals, paths)
-    animation.save("outputs/result.mp4", 1.0)
+    animation.save("../outputs/result.mp4", 1.0)
